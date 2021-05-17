d721279ec5d2035a6d3b59308169ff4e19405614
M. Solana Castillo Martinez <solichuli333@gmail.com>
2021-05-17 11:35:15 -0300

    Arreglo un par de cosas

--- Castillo-GarciaDaistugue-Medina-TP3/cAlquileres.h
+++ Castillo-GarciaDaistugue-Medina-TP3/cAlquileres.h
@@ -1,9 +1,11 @@
 #pragma once
+#include "cListaT.h"
+#include "cAlquiler.h"
 #include <sstream>
 #include <string>
 using namespace std;
 class cAlquileres :
-    public cListaT
+    public cListaT<cAlquiler>
 {
 private:
     int GananciaTotal;
