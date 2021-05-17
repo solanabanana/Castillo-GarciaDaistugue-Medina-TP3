3bfa0ad8be28904f592ac8b2580d0380d323761e
Luiselys Medina <mluiselys@gmail.com>
2021-05-17 11:41:15 -0300

    creacion de definicion en el cpp

--- Castillo-GarciaDaistugue-Medina-TP3/cAlquileres.cpp
+++ Castillo-GarciaDaistugue-Medina-TP3/cAlquileres.cpp
@@ -1,1 +1,24 @@
 #include "cAlquileres.h"
+
+cAlquileres::cAlquileres()
+{
+
+}
+
+cAlquileres::~cAlquileres()
+{
+}
+
+int cAlquileres::CalcularGananciaTotal()
+{
+	return 0;
+}
+
+cAlquileres* cAlquileres::OrdenarPorTipoVehiculo(string tipo_vehiculo)
+{
+	return nullptr;
+}
+
+void cAlquileres::ListarPorTipoVehiculo(cAlquileres* alquileres)
+{
+}
